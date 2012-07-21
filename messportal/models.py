33367a15285b863055d1a3429a4d990bd24d657f
Praveen Venkatesh <praveenv253@gmail.com>
2012-07-21 12:05:42 +0530

    Added UserProfile model with feedback field

--- messportal/models.py
+++ messportal/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from django.db                      import models
+from django.contrib.auth.models     import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    """
+    Model to contain feedback information of previous mess experience.
+    """
+    user = models.ForeignKey(User)
+    
+    # Rating for each feedback field is encoded into a single integer field
+    # For example:
+    #       Hygiene   - 3
+    #       Quantity  - 4
+    #       Quality   - 3
+    #       Overall   - 3
+    # will be encoded as 3433
+    feedback = models.IntegerField(blank = False, null = True)
+    
+    def __unicode__(self):
+        return unicode(user)
+
