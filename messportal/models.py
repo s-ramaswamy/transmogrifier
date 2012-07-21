964a8dd21603d9886b102aeadf6f4642e686dd1c
Praveen Venkatesh <praveenv253@gmail.com>
2012-07-21 13:32:51 +0530

    1. Added a limit field to the Caterer model 2. Added a related name to the FK from UserProfile to Caterer 3. Added a get_list_of_caterers function that will be called by the form for available choices

--- messportal/models.py
+++ messportal/models.py
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
     # will be encoded as 3433
     feedback = models.IntegerField(blank = False, null = True)
     
-    choice_of_caterer = models.ForeignKey(Caterer)
+    choice_of_caterer = models.ForeignKey(Caterer, related_name = 'students')
     
     def __unicode__(self):
         return unicode(user)
@@ -30,7 +30,22 @@ class Caterer(models.Model):
     choices for users to select from.
     """
     name = models.CharField(max_length = 100)
+    limit = models.IntegerField()
+    # 'students' is a related field - FK from UserProfile
     
     def __unicode__(self):
         return unicode(self.name)
 
+
+def get_list_of_caterers():
+    """
+    Returns a list of caterers for the current implementaton.
+    This list will be displayed directly on the template, therefore it must
+    pass basic tests such as limit-checking.
+    """
+    list_of_caterers = []
+    for caterer in Caterer.objects.all():
+        # Check if number of students registered already is less than limit
+        if caterer.limit > caterer.students.count():
+            list_of_caterers.append(caterer)
+    return list_of_caterers
