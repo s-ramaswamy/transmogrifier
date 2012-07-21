d13fae1159243c018a6d2d085ee3340811b06f74
Swaroop Ramaswamy <swaroop551992@gmail.com>
2012-07-22 02:17:05 +0530

    Using the urls.py from master and the other branch (one_caterers_table)

--- urls.py
+++ urls.py
@@ -1,3 +1,22 @@
-from messportal.urls import urlpatterns as messportal_urlpatterns
+from django.conf.urls.defaults import patterns, url, include
+from django.contrib            import admin
 
-urlpatterns = messportal_urlpatterns
+from transmogrifier.settings import MEDIA_ROOT
+from messportal import urls
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+        
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve',
+            { 'document_root': MEDIA_ROOT }),
+
+        url(r'^admin/',
+            include(admin.site.urls)
+            ),
+
+        url(r'^',
+            include('transmogrifier.messportal.urls')
+            ),
+        )
